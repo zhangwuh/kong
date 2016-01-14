92ce494c20e692611ee47d6d6399ed287ded2157
thefosk <marco@mashape.com>
2016-01-14 14:48:44 -0800

    consensus timeout

--- kong/dao/cassandra/factory.lua
+++ kong/dao/cassandra/factory.lua
@@ -119,6 +119,9 @@ function CassandraFactory:get_session_options()
     query_options = {
       prepare = true
     },
+    protocol_options = {
+      max_schema_consensus_wait = 15000
+    },
     username = self.properties.username,
     password = self.properties.password,
     ssl_options = {
